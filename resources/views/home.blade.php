eb5f702b7e37194d6018f6b747c0e86d0d071065
GLCVLL <g.ioele96@outlook.it>
2023-08-21 19:22:44 +0200

    added header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dc Comics</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <header>
+        <div class="container" id="navbar-header">
+            <figure class="logo">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+            </figure>
+            <ul>
+                <li><a href="{{ route('characters') }}">Characters</a></li>
+                <li><a href="{{ route('comics') }}">Comics</a></li>
+                <li><a href="{{ route('movies') }}">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </div>
+    </header>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// rotta per pagina home
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// rotta per pagina Characters
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+// rotta per pagina Comics
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+// rotta per pagina movies
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
